efa749380e25367f42d0b1f26375428b0417411a
S Praveen <108536707+Spraveen8-chary@users.noreply.github.com>
2024-08-30 21:54:49 +0530

    Update app.py

--- LOAN_PREDICTION/app.py
+++ LOAN_PREDICTION/app.py
@@ -5,7 +5,6 @@ import pandas as pd
 
 app = Flask(__name__)
 
-# Load the saved model
 with open('model.pickle', 'rb') as f:
     model = pickle.load(f)
 
@@ -13,7 +12,6 @@ with open('model.pickle', 'rb') as f:
 @app.route('/',  methods=['GET', 'POST'])
 def predict():
     if request.method == 'POST':
-        # Get user inputs from the form
         Gender = int(request.form['Gender'])
         Married = int(request.form['Married'])
         Dependents = int(request.form['Dependents'])
@@ -31,19 +29,17 @@ def predict():
         LoanAmountLog = np.log(LoanAmountLog)
         Loan_Amount_Term_Log = np.log(Loan_Amount_Term_Log)
         Total_Income_Log = np.log(Total_Income_Log)
-        # Create a feature vector
+        
         input_data = np.array([[
             Gender, Married, Dependents, Education, Self_Employed,
             Credit_History, Property_Area, ApplicantIncomeLog,
             LoanAmountLog, Loan_Amount_Term_Log, Total_Income_Log
         ]])
 
-        # Make a prediction using the model
         prediction = model.predict(pd.DataFrame(input_data))
         predicted_labels = ['Approved' if prediction == 1 else 'Not Approved' for prediction in prediction]
 
 
-        # Render the prediction result in a simple HTML page
         
         return render_template('index.html',predicted_labels = predicted_labels[0])
     return render_template('index.html',predicted_labels = None)
